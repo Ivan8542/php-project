3759b0b58ea59a6dfa0717af362df4034242fc8d
Ivan Syryhin <isyrygin81@gmail.com>
2023-02-03 22:42:24 +0200

    edit file

--- TelegraphText.php
+++ TelegraphText.php
@@ -14,7 +14,6 @@ class TelegraphText
         $this->slug = $slug;
         $this->published = date('d.m.Y H:i:s');
     }
-
     public function storeText()
     {
         $array = [
@@ -25,13 +24,10 @@ class TelegraphText
             "slug" => $this->slug,
         ];
 
-
         return file_put_contents($this->slug, serialize($array));
     }
-
     public function loadText()
     {
-
         $array = unserialize(file_get_contents($this->slug, false));
 
         $this->title = $array['title'];
@@ -41,13 +37,87 @@ class TelegraphText
 
         return $this->text;
     }
-
     public function editText($title, $text)
     {
         $this->title = $title;
         $this->text = $text;
     }
+}
 
+abstract class User
+{
+    public $id, $name, $role;
+    abstract public function getTextsToEdit();
+}
+abstract class View
+{
+    public $storage;
+
+    public function __construct($storage)
+    {
+        $this->storage = $storage;
+    }
+    abstract public function displayTextById($id);
+    abstract public function displayTextByUrl($url);
+}
+
+abstract class Storage
+{
+    abstract public function create($object);
+    abstract public function read($slug);
+    abstract public function update($slug);
+    abstract public function delete($slug);
+    abstract public function list();
+}
+class FileStorage extends Storage
+{
+    public $dir = '/xampp/htdocs/welcome/';
+    public function create(TelegraphText $object)
+    {
+        $file = new $object;
+        $serialize = serialize($file);
+
+        $i = 1;
+        $slug = $object->slug . '_' . $i . '.txt';
+        while(file_exists($this->dir . $slug)) {
+            $i += 1;
+        }
+
+        return $slug;
+    }
+    public function read($slug)
+    {
+        if (file_exists($slug)) {
+            $object = unserialize(file_get_contents($slug, false));
+            return $object;
+        }
+        return false;
+    }
+    public function update($object, $slug)
+    {
+        if (file_exists($slug)) {
+            file_put_contents($slug, serialize(get_object_vars($object)));
+            return true;
+        }
+        return false;
+    }
+    public function list()
+    {
+        $scandir = scandir($this->dir);
+        foreach ($scandir as $s) {
+
+        }
+
+    }
+    public function delete($slug)
+    {
+        if (file_exists($slug)) {
+            unlink($slug);
+            return true;
+        }
+
+        return false;
+    }
 }
 
 $telegraphText = new TelegraphText('Иван', 'text.txt');
